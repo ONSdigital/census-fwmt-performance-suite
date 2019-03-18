ea2827029110cb288c9ccae206f6a1a52da1fc6f
stevescorfield <steve.scorfield@ons.gov.uk>
2019-03-18 14:26:52 +0000

    Initial commit

---
+++ src/main/java/uk/gov/ons/census/fwmt/performancesuite/PerformancesuiteApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.ons.census.fwmt.performancesuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformancesuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformancesuiteApplication.class, args);
+	}
+
+}
