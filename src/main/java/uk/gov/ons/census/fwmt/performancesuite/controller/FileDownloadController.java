e883e21024b1d8965620140737061a7e5990c660
jacobharrison <jacob.harrison@qa.com>
2019-04-05 11:56:50 +0100

    added file ownload controller

--- src/main/java/uk/gov/ons/census/fwmt/performancesuite/controller/FileDownloadController.java
+++ src/main/java/uk/gov/ons/census/fwmt/performancesuite/controller/FileDownloadController.java
@@ -1,22 +1,36 @@
 package uk.gov.ons.census.fwmt.performancesuite.controller;
 
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @Controller
 public class FileDownloadController {
 
-  @RequestMapping("/downloadCsv")
-  public StreamingResponseBody downloadCsv() {
-    return outputStream -> {
-      File file = new File("src/main/resources/csv/Performance_Test_CSV.csv");
-      FileInputStream stream = new FileInputStream(file);
-      IOUtils.copy(stream, outputStream);
-    };
+  @GetMapping("/downloadReport")
+  public ResponseEntity<byte[]> downloadReportData() throws IOException {
+    File file = new File("src/main/resources/report/Performance_Test_Report.txt");
+    final byte[] fileBytes = Files.readAllBytes(file.toPath());
+    String fileName = "src/main/resources/report/Performance_Test_Report.txt";
+    HttpHeaders respHeaders = new HttpHeaders();
+    respHeaders.setContentType(MediaType.TEXT_PLAIN);
+    respHeaders.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+    return new ResponseEntity<>(fileBytes,respHeaders, HttpStatus.OK);
+  }
+
+  @GetMapping("/downloadCsv")
+  public ResponseEntity downloadCSVData() throws IOException {
+    File file = new File("src/main/resources/csv/Performance_Test_CSV.csv");
+      final byte[] fileBytes = Files.readAllBytes(file.toPath());
+      final ByteArrayResource resource = new ByteArrayResource(fileBytes);
+      return ResponseEntity.ok().contentLength(fileBytes.length).contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
   }
 }
