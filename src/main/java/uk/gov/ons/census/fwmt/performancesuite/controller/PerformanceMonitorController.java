cd69d56dcea4dc00666c26496d39c8ff22bfbe6a
stevescorfield <steve.scorfield@ons.gov.uk>
2019-04-12 13:22:33 +0100

    Update to remove message queue after usage.

--- src/main/java/uk/gov/ons/census/fwmt/performancesuite/controller/PerformanceMonitorController.java
+++ src/main/java/uk/gov/ons/census/fwmt/performancesuite/controller/PerformanceMonitorController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import uk.gov.ons.census.fwmt.performancesuite.components.GatewayPerformanceMonitor;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeoutException;
 
 @Controller
@@ -16,8 +18,22 @@ public class PerformanceMonitorController {
 
   @Value("${rabbitmq.rabbitLocation}")
   String rabbitLocation;
+  @Value("${storage.csvLocation}")
+  String csvLocation;
+  @Value("${storage.reportLocation}")
+  String reportLocation;
   @Autowired
   private GatewayPerformanceMonitor gatewayPerformanceMonitor;
+
+  @GetMapping("/deleteCSV")
+  public void  deleteCSV() throws IOException {
+    Files.deleteIfExists(Paths.get(csvLocation));
+  }
+
+  @GetMapping("/deleteReport")
+  public void deleteReport() throws IOException {
+    Files.deleteIfExists(Paths.get(reportLocation));
+  }
 
   @GetMapping("/enablePerformanceMonitor")
   public String enablePerformanceMonitor(@RequestParam("expectedJobs") int expectedJobs)
